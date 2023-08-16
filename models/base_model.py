e6d830485059062010417149d8048376757fb1bd
rubio91147 <rubendariofb@gmail.com>
2023-08-16 15:16:59 -0500

    . Improve engines

--- models/base_model.py
+++ models/base_model.py
@@ -6,28 +6,28 @@ import models
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
 
-
 class BaseModel:
-    """A base class for all hbnb models"""
-    id = Column(String(60), primary_key=True, unique=True)
-    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
+    """Some doc"""
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(
+        DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(
+        DateTime, default=datetime.utcnow(), nullable=False)
     def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
+        """Some doc"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.utcnow()
         if kwargs:
-            for k, v in kwargs.items():
-                if k in ['created_at', 'updated_at']:
-                    t_format = '%Y-%m-%dT%H:%M:%S.%f'
-                    self.__dict__[k] = datetime.strptime(v, t_format)
-                elif k != '__class__':
-                    setattr(self, k, v)
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                self.__dict__[key] = value
 
     def __str__(self):
         """Returns a string representation of the instance"""
